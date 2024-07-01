67c0558dcab7a86f635f4c1624a173d3cb85f983
devikaks05 <sng22cs063@sngce.ac.in>
2024-07-01 16:30:53 +0530

    yes

---
+++ operator.js
@@ -0,0 +1,26 @@
+//arithmetic operators
+let a=30;
+let b=10;
+let c=a+b;
+let d=a-b;
+let e=a*b;
+let f=a/b;
+let g=a%b;
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+
+//postfix  increment
+let p=q;
+let q=p++;
+console.log(p);
+console.log(q);
+
+//prefix increment
+let m=2;
+let n=++m;
+console.log(m);
+console.log(n);
+
